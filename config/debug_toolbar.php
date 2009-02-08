36453637bb934412353bb98a068c09f20bbb66ff
pifantastic <pifantastic@20442493-6d62-0410-bc4e-a408091d383b>
2009-02-08 22:56:22 +0000

    git-svn-id: http://svn.afloorabove.com/kohanadebugtoolbar/trunk@13 20442493-6d62-0410-bc4e-a408091d383b

--- config/debug_toolbar.php
+++ config/debug_toolbar.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Location of icon images, include trailing slash
-$config['icon_path'] = '/Kohana_v2.3.1/images/';
+$config['icon_path'] = '/images/';
 
 ?>
